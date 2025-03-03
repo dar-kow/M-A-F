304eebb39914757d0a5dd52147a18075e7f5c907
Darek <darecki9k@gmail.com>
2025-03-03 05:11:58 +0100

    Remove unused utility functions for number conversion, money formatting, VAT rate retrieval, and font registration to streamline the project

--- frontend/src/utils/fonts/OpenSans.ts
+++
@@ -1,6 +0,0 @@
-import jsPDF from "jspdf";
-
-export function registerOpenSans(doc: jsPDF) {
-  doc.addFileToVFS("OpenSans.ttf", "<base64>");
-  doc.addFont("OpenSans.ttf", "OpenSans", "normal");
-}
