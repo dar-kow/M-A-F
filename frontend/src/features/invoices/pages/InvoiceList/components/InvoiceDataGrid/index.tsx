0aa91f4b72ead0d651e524f6cccc71502ef658ba
Darek <darecki9k@gmail.com>
2025-03-10 06:07:48 +0100

    Add InvoiceDataGrid component and export related hooks for reuse

---
+++ frontend/src/features/invoices/pages/InvoiceList/components/InvoiceDataGrid/index.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable react-refresh/only-export-components */
+import InvoiceDataGrid from './InvoiceDataGrid';
+
+export default InvoiceDataGrid;
+
+// Eksport typów i hooków dla ponownego użycia
+export * from './hooks/useColumnFilters';
+export * from './hooks/useColumnManagement';
+export * from './hooks/useAutoHeight';
+export * from './hooks/useDelayedLoading';
