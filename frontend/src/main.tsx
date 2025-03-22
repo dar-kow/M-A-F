e2137a5e035d1fb130be57e5840c77ebf7ef408f
Darek <darecki9k@gmail.com>
2025-03-22 13:24:25 +0100

    Add page tracking functionality using Google Analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import App from './App';
 import store from './store/store';
 import '@styles/global.scss';
+import { trackPageView } from './analytics';
+
+const PageTracker: React.FC = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        trackPageView(location.pathname + location.search);
+    }, [location]);
+
+    return null;
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
+                <PageTracker />
                 <App />
             </BrowserRouter>
         </Provider>
