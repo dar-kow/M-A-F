3a847fec2ffef461bc478c91065d5d09c737546a
Darek <darecki9k@gmail.com>
2025-04-15 20:34:55 +0200

    Remove unnecessary comments and eslint directive in InvoiceDataGrid index file

--- frontend/src/features/invoices/pages/InvoiceList/components/InvoiceDataGrid/index.tsx
+++ frontend/src/features/invoices/pages/InvoiceList/components/InvoiceDataGrid/index.tsx
@@ -1,9 +1,7 @@
-/* eslint-disable react-refresh/only-export-components */
 import InvoiceDataGrid from './InvoiceDataGrid';
 
 export default InvoiceDataGrid;
 
-// Eksport typów i hooków dla ponownego użycia
 export * from './hooks/useColumnFilters';
 export * from './hooks/useColumnManagement';
 export * from './hooks/useAutoHeight';
